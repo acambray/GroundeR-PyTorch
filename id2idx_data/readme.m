59bd05b15445f98dec734d7da3354a361f78c3b3
Aleix Cambray <acambray95@gmail.com>
2019-04-11 23:36:29 +0100

    initial commit

---
+++ id2idx_data/readme.m
@@ -0,0 +1,3 @@
+from process_dataset.py
+
+Each file in this folder corresponds to an image of the dataset and contains a dictionary which maps an object_ID to its corresponding index in the visual feature matrix.
